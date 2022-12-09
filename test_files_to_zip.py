277a078a652fab8b3b96a038b20c7b471548ada0
Rammsat <saturn9911@mail.ru>
2022-12-09 17:42:50 +0300

    Создан тест для архива

--- test_files_to_zip.py
+++ test_files_to_zip.py
@@ -1,9 +1,10 @@
 from zipfile import ZipFile
 from os.path import basename
+from PyPDF2 import PdfReader
 import os
 
+file_direction = (os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources/zip_file.zip'))
 
-file_diraction = (os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources/zip_file.zip'))
 
 #  Созание архива
 with ZipFile('resources/zip_file.zip', 'w') as zip_file:
@@ -16,20 +17,21 @@ with ZipFile('resources/zip_file.zip', 'w') as zip_file:
 
 #   Тест для xls
 def test_read_xls_from_zip():
-    with ZipFile(file_diraction) as zip_files:
-        text = str(zip_files.read('some_xls.xls'))
+    with ZipFile(file_direction) as zip_file:
+        text = str(zip_file.read('some_xls.xls'))
         assert text.__contains__('321')
 
 
 #   Тест для csv
 def test_read_csv_from_zip():
-    with ZipFile(file_diraction) as zip_files:
-        text = str(zip_files.read('some_csv.csv'))
+    with ZipFile(file_direction) as zip_file:
+        text = str(zip_file.read('some_csv.csv'))
         assert text.__contains__('csv')
 
 
 #   Тест для pdf
 def test_read_pdf_from_zip():
-    with ZipFile((os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources/zip_file.zip'))) as zip_files:
-        text = str(zip_files.read('some_pdf.pdf'))
-        assert text.__contains__('PDF Test File')
+    with ZipFile(file_direction) as zip_file:
+        zip_file.extract('some_pdf.pdf')
+        text = PdfReader('some_pdf.pdf').pages[0].extract_text()
+        assert text.__contains__('Dumm y PDF file')
