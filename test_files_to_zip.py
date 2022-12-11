b0dddeae014245303076edd6f0695d74cef7fb80
Rammsat <saturn9911@mail.ru>
2022-12-11 15:52:39 +0300

    Добавлено удаление pdf

--- test_files_to_zip.py
+++ test_files_to_zip.py
@@ -3,10 +3,11 @@ from os.path import basename
 from PyPDF2 import PdfReader
 import os
 
+
 file_direction = (os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources/zip_file.zip'))
 
 
-#  Созание архива
+#  Создание архива
 with ZipFile('resources/zip_file.zip', 'w') as zip_file:
     for folder_name, sub_folders, file_names in \
             os.walk((os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources/files'))):
@@ -15,23 +16,21 @@ with ZipFile('resources/zip_file.zip', 'w') as zip_file:
             zip_file.write(file_path, basename(file_path))
 
 
-#   Тест для xls
 def test_read_xls_from_zip():
     with ZipFile(file_direction) as zip_file:
         text = str(zip_file.read('some_xls.xls'))
         assert text.__contains__('321')
 
 
-#   Тест для csv
 def test_read_csv_from_zip():
     with ZipFile(file_direction) as zip_file:
         text = str(zip_file.read('some_csv.csv'))
         assert text.__contains__('csv')
 
 
-#   Тест для pdf
 def test_read_pdf_from_zip():
     with ZipFile(file_direction) as zip_file:
         zip_file.extract('some_pdf.pdf')
         text = PdfReader('some_pdf.pdf').pages[0].extract_text()
+        os.remove('some_pdf.pdf')
         assert text.__contains__('Dumm y PDF file')
